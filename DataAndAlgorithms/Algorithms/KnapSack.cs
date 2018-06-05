aa8a51da49321007c50db16a0ef39fe560d11dda
gusAGR <gustavo.a.garcia.rod@outlook.com>
2018-06-05 08:15:53 +0200

    Modify documentation comments

--- DataAndAlgorithms/Algorithms/KnapSack.cs
+++ DataAndAlgorithms/Algorithms/KnapSack.cs
@@ -14,7 +14,7 @@ namespace Algorithms
         /// Gets de maximun benefit and the volumes of the elements included in the knapsack which solve the problem.
         /// </summary>
         /// <param name="maxWeight">Maximun weight allowed in the knapsack</param>
-        /// <param name="volumes">Array with the volumes of the elements.</param>
+        /// <param name="volumes">Array with the element volumes.</param>
         /// <param name="benefits">Array with the benefit of each element. The n benefit elements corresponds with the n volumes element</param>
         /// <param name="numberOfElements">Number of elements</param>
         /// <returns> Tuple </returns>
@@ -39,7 +39,10 @@ namespace Algorithms
             return (maxBenefit, elements);
         }
 
-
+        /// <summary>
+        /// Generate a matrix with all the possible solutions.
+        /// </summary>
+        /// <returns>Two dimension array of ints</returns>
         public int[,] GenerateMatrix(int maxWeight, int[] volumes, int[] benefits, int numberOfElements) {
 
             int[,] benefitsMatrix = new int[numberOfElements +1, maxWeight +1];
